8a0db320e7a520d4f3fec0b083c2ef508adc4900
Karan Gajjar <karangajjar.lp@gmail.com>
2022-01-06 23:44:57 -0500

    remove / from api

--- src/Components/utils/const.js
+++ src/Components/utils/const.js
@@ -1,5 +1,5 @@
 const Constants = {
-    LOCALURL: "http://127.0.0.1:8000/api/",
-    APIURL: "http://10.40.4.16:8000/api/"
+    LOCALURL: "http://127.0.0.1:8000/api",
+    APIURL: "http://10.40.4.16:8000/api"
 }
 export default Constants
